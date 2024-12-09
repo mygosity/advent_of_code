add0d3dfc60dcb617d539dfc917b7158697aada7
mygosity <mygosity@gmail.com>
2024-12-09 20:59:16 +1100

    Added solution to advent of code part 2 for day 9

--- adventofcode/2024/src/09.js
+++ adventofcode/2024/src/09.js
@@ -11,7 +11,7 @@ function solveData(data) {
     const numIds = parseInt(data[i]);
     idValues.push([id++, numIds, freeSpace]);
   }
-  console.log({ idValues });
+  // console.log({ idValues });
 
   let checkSum = 0n;
 
@@ -27,7 +27,7 @@ function solveData(data) {
     for (let i = 0; i < numOfIds; ++i) {
       index = snapShottedIndex + i;
       checkSum += BigInt(index * id);
-      console.log({ index, id, checkSum, freeSpace });
+      // console.log({ index, id, checkSum, freeSpace });
     }
 
     if (numOfIds > 0) {
@@ -53,7 +53,7 @@ function solveData(data) {
         index = snapShottedIndex + i;
         const nextId = getNextIdFromRight();
         checkSum += BigInt(index * nextId);
-        console.log({ index, nextId, checkSum });
+        // console.log({ index, nextId, checkSum });
       }
       index++;
     }
@@ -64,11 +64,87 @@ function solveData(data) {
   return checkSum;
 }
 
+function solveDataB(data) {
+  // console.log(data);
+  const idValues = [];
+  let id = 0;
+  for (let i = 0; i < data.length; i += 2) {
+    const freeSpace = parseInt(data[i + 1] ?? "0");
+    const numIds = parseInt(data[i]);
+    idValues.push([id++, numIds, freeSpace, [], numIds]);
+  }
+  // console.log({ idValues });
+
+  //reorder the right hand side until all free space is used
+  for (let j = idValues.length - 1; j > 0; --j) {
+    const [id, numOfIds, freeSpace, captured] = idValues[j];
+    for (let i = 0; i < j; ++i) {
+      const [otherId, otherNumOfIds, otherFreeSpace, otherCaptured] =
+        idValues[i];
+      if (numOfIds <= otherFreeSpace) {
+        //zero these as they've moved
+        idValues[j][1] = 0;
+        //subtract from the capturing group that will allow these freeloaders
+        idValues[i][2] -= numOfIds;
+        otherCaptured.push([id, numOfIds]);
+        break;
+      }
+    }
+  }
+
+  let checkSum = 0n;
+
+  //pts to next file block to use to fill in
+  let index = 0;
+
+  // console.log(idValues);
+
+  for (let left = 0; left < idValues.length; ++left) {
+    let [id, numOfIds, freeSpace, captured, originalNum] = idValues[left];
+
+    if (numOfIds > 0) {
+      for (let i = 0; i < numOfIds; ++i) {
+        checkSum += BigInt(index * id);
+        // console.log({ index, id, checkSum, freeSpace });
+        index++;
+      }
+    } else {
+      //tricky edge condition to remember where the number that moved
+      // leaves an empty space equivalent to how much was there
+      for (let i = 0; i < originalNum; ++i) {
+        // console.log({ index });
+        index++;
+      }
+    }
+
+    if (captured.length) {
+      for (let i = 0; i < captured.length; ++i) {
+        const [innerId, innerNum] = captured[i];
+        for (let j = 0; j < innerNum; ++j) {
+          checkSum += BigInt(index * innerId);
+          // console.log({ index, innerId, innerNum, checkSum });
+          index++;
+        }
+      }
+    }
+
+    if (freeSpace > 0) {
+      for (let i = 0; i < freeSpace; ++i) {
+        // console.log({ index, checkSum });
+        index++;
+      }
+    }
+  }
+
+  return checkSum;
+}
+
 async function solveAdventPuzzle() {
   const file = currInputPath + "09.txt";
   const data = fs.readFileSync(file).toString();
 
   let sum = solveData(data);
-  console.log({ sum });
+  let sumB = solveDataB(data);
+  console.log({ sum, sumB });
 }
 solveAdventPuzzle();
