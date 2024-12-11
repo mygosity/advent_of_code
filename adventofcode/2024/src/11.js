fd967e427af0a5264ea4b4f2a62ee4ef2b2dfb45
mygosity <mygosity@gmail.com>
2024-12-12 10:44:27 +1100

    Added DP solution to problem 11 part b

--- adventofcode/2024/src/11.js
+++ adventofcode/2024/src/11.js
@@ -4,42 +4,49 @@ console.log(`******************\n`);
 
 function solve(data) {
   let stones = data.split(" ");
-  console.log({ stones });
+  // console.log({ stones });
 
-  for (let i = 0; i < 25; ++i) {
-    let nextSet = [];
-    for (const stoneStr of stones) {
-      // console.log({ stoneStr });
+  const cache = {};
+  let totalSum = 0n;
+  const targetIterations = 75;
 
-      if (stoneStr === "0") {
-        nextSet.push("1");
-        continue;
-      }
-
-      if (stoneStr.length % 2 === 0) {
-        const firstPart = stoneStr.substring(0, stoneStr.length >> 1);
-        let secondPart = stoneStr.substring(stoneStr.length >> 1);
-        let secondIndex = 0;
-        while (
-          secondIndex < secondPart.length - 1 &&
-          secondPart[secondIndex] === "0"
-        ) {
-          secondIndex++;
-        }
-        secondPart = secondPart.substring(secondIndex);
-        // console.log({ firstPart, secondPart });
-        nextSet.push(firstPart, secondPart);
-        continue;
-      }
-
-      const nextNum = (BigInt(stoneStr) * BigInt(2024)).toString();
-      nextSet.push(nextNum);
+  function dfs(stoneStr, i) {
+    if (i === targetIterations) {
+      return 1n;
     }
-    stones = nextSet;
-    console.log({ i });
-    // console.log({ i });
+    if (cache[stoneStr]?.[i] != null) {
+      return cache[stoneStr][i];
+    }
+    cache[stoneStr] = cache[stoneStr] || {};
+    let currSum = 0n;
+
+    if (stoneStr === "0") {
+      currSum += dfs("1", i + 1);
+    } else if (stoneStr.length % 2 === 0) {
+      const firstPart = stoneStr.substring(0, stoneStr.length >> 1);
+      let secondPart = stoneStr.substring(stoneStr.length >> 1);
+      let secondIndex = 0;
+      while (
+        secondIndex < secondPart.length - 1 &&
+        secondPart[secondIndex] === "0"
+      ) {
+        secondIndex++;
+      }
+      secondPart = secondPart.substring(secondIndex);
+      currSum += dfs(firstPart, i + 1);
+      currSum += dfs(secondPart, i + 1);
+    } else {
+      const nextNum = (BigInt(stoneStr) * BigInt(2024)).toString();
+      currSum += dfs(nextNum, i + 1);
+    }
+    return (cache[stoneStr][i] = currSum);
   }
-  return stones.length;
+
+  for (const stone of stones) {
+    totalSum += dfs(stone, 0);
+  }
+
+  return totalSum;
 }
 
 async function solveAdventPuzzle() {
