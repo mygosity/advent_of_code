1ace36f38cef326498f1d9d4033e17d756c9085e
mygosity <mygosity@gmail.com>
2024-12-12 15:45:11 +1100

    Added iterative dp solutions and additional challenge data.

--- adventofcode/2024/src/11.js
+++ adventofcode/2024/src/11.js
@@ -2,13 +2,111 @@ const currInputPath = paths.adventOfCode + "/2024/inputs/";
 console.log(`\n******************\nLoaded 2024/11.js`);
 console.log(`******************\n`);
 
-function solve(data) {
+function solveByTabulationOptimized(data, targetIterations) {
+  const stones = data.split(" ");
+
+  let prevDp = new Map();
+  for (const stone of stones) {
+    prevDp.set(stone, (prevDp.get(stone) || 0n) + 1n);
+  }
+
+  for (let i = 0; i < targetIterations; i++) {
+    let nextDp = new Map();
+
+    for (const [stoneStr, count] of prevDp.entries()) {
+      let nextStates;
+      if (stoneStr === "0") {
+        nextStates = ["1"];
+      } else if (stoneStr.length % 2 === 0) {
+        const half = stoneStr.length >> 1;
+        let firstPart = stoneStr.substring(0, half);
+        let secondPart = stoneStr.substring(half);
+
+        let secondIndex = 0;
+        while (
+          secondIndex < secondPart.length - 1 &&
+          secondPart[secondIndex] === "0"
+        ) {
+          secondIndex++;
+        }
+        secondPart = secondPart.substring(secondIndex);
+
+        nextStates = [firstPart, secondPart];
+      } else {
+        const nextNum = (BigInt(stoneStr) * 2024n).toString();
+        nextStates = [nextNum];
+      }
+
+      for (const ns of nextStates) {
+        nextDp.set(ns, (nextDp.get(ns) || 0n) + count);
+      }
+    }
+    prevDp = nextDp;
+  }
+
+  let totalSum = 0n;
+  for (const val of prevDp.values()) {
+    totalSum += val;
+  }
+
+  return totalSum;
+}
+
+function solveByTabulation(data, targetIterations) {
+  const stones = data.split(" ");
+
+  let dp = [new Map()];
+  for (const stone of stones) {
+    dp[0].set(stone, (dp[0].get(stone) || 0n) + 1n);
+  }
+
+  for (let i = 0; i < targetIterations; i++) {
+    dp[i + 1] = new Map();
+
+    for (const [stoneStr, count] of dp[i].entries()) {
+      let nextStates;
+      if (stoneStr === "0") {
+        nextStates = ["1"];
+      } else if (stoneStr.length % 2 === 0) {
+        const half = stoneStr.length >> 1;
+        let firstPart = stoneStr.substring(0, half);
+        let secondPart = stoneStr.substring(half);
+
+        let secondIndex = 0;
+        while (
+          secondIndex < secondPart.length - 1 &&
+          secondPart[secondIndex] === "0"
+        ) {
+          secondIndex++;
+        }
+        secondPart = secondPart.substring(secondIndex);
+
+        nextStates = [firstPart, secondPart];
+      } else {
+        const nextNum = (BigInt(stoneStr) * 2024n).toString();
+        nextStates = [nextNum];
+      }
+
+      for (const ns of nextStates) {
+        dp[i + 1].set(ns, (dp[i + 1].get(ns) || 0n) + count);
+      }
+    }
+  }
+
+  let totalSum = 0n;
+  for (const val of dp[targetIterations].values()) {
+    totalSum += val;
+  }
+
+  return totalSum;
+}
+
+function solve(data, targetIterations) {
   let stones = data.split(" ");
   // console.log({ stones });
 
   const cache = {};
   let totalSum = 0n;
-  const targetIterations = 75;
 
   function dfs(stoneStr, i) {
     if (i === targetIterations) {
@@ -53,7 +151,13 @@ async function solveAdventPuzzle() {
   const file = currInputPath + "11.txt";
   const data = fs.readFileSync(file).toString();
 
-  let sum = solve(data);
-  console.log({ sum });
+  const smallIterations = 25;
+  const bigIterations = 75;
+  const challengeIterations = 100;
+  console.log({
+    sumA: solve(data, smallIterations), //runs out of memory at 5003
+    sumB: solveByTabulation(data, bigIterations), //runs out of memory at 13666
+    sumC: solveByTabulationOptimized(data, challengeIterations), //can solve +50000
+  });
 }
 solveAdventPuzzle();
