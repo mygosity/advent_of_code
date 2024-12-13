4bb0e87a0c44c47cdf5ed14de6bd2bba0d303d44
mygosity <mygosity@gmail.com>
2024-12-13 18:55:30 +1100

    Added solution to part a day 13.

--- adventofcode/2024/src/13.js
+++ adventofcode/2024/src/13.js
@@ -3,8 +3,49 @@ const fileTargetNumber = 13;
 console.log(`\n******************\nLoaded 2024/${fileTargetNumber}.js`);
 console.log(`******************\n`);
 
+function extractCoordinates(line, char) {
+  const [axSet, aySet] = line.split(",");
+  const [_a, axStr] = axSet.split(char);
+  const [__a, ayStr] = aySet.split(char);
+  const ax = parseInt(axStr);
+  const ay = parseInt(ayStr);
+  return [ax, ay];
+}
+
 function solve(data) {
   let sum = 0n;
+
+  function dfs(cache, x, y, ax, ay, bx, by, tx, ty) {
+    if (x > tx && y > ty) {
+      return Infinity;
+    }
+    if (x === tx && y === ty) {
+      return 0;
+    }
+    const key = x + "|" + y;
+    if (cache[key] != null) {
+      return cache[key];
+    }
+    const cost = Math.min(
+      3 + dfs(cache, x + ax, y + ay, ax, ay, bx, by, tx, ty),
+      1 + dfs(cache, x + bx, y + by, ax, ay, bx, by, tx, ty)
+    );
+    return (cache[key] = cost);
+  }
+
+  for (const puzzleSet of data) {
+    let [lineAStr, lineBStr, prizeLocationStr] = puzzleSet;
+    const [ax, ay] = extractCoordinates(lineAStr.split(":")[1], "+");
+    const [bx, by] = extractCoordinates(lineBStr.split(":")[1], "+");
+    const [tx, ty] = extractCoordinates(prizeLocationStr.split(":")[1], "=");
+
+    // console.log({ ax, ay, bx, by, tx, ty });
+
+    const cache = {};
+    const answer = dfs(cache, 0, 0, ax, ay, bx, by, tx, ty);
+    sum += BigInt(answer === Infinity ? 0 : answer);
+  }
+
   return sum;
 }
 
@@ -14,10 +55,19 @@ async function solveAdventPuzzle() {
 
   const lines = data.split("\n");
 
+  const puzzles = [];
+  let puzzleIndex = 0;
   for (const line of lines) {
+    puzzles[puzzleIndex] = puzzles[puzzleIndex] || [];
+    if (line === "") {
+      puzzleIndex++;
+      continue;
+    }
+    puzzles[puzzleIndex].push(line);
   }
+  // console.log(puzzles);
 
-  const sum = solve(data);
+  const sum = solve(puzzles);
   console.log({ sum });
 }
 solveAdventPuzzle();
